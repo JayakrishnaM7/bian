01a369aad9e62f60cae53224ffecc220f504181f
Jayakrishna.Mudimanchi <jayakrishna.mudimanchi@ibm.com>
2022-09-28 12:33:53 +0530

    added controller

---
+++ src/main/java/com/ibm/org/controller/FundtransferController.java
@@ -0,0 +1,26 @@
+package com.ibm.org.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 0031CY744
+ *
+ */
+@RestController
+@RequestMapping("v1/bian-3/core/payment/creditor/10.0.0/")
+public class FundtransferController {
+
+	
+
+	
+
+	@PostMapping
+	public void getStudent() {
+		System.out.println("");
+	}
+
+	
+
+}
