1dee3bbeed8784183083cbd964f1392a0eb9d2d3
Jayakrishna.Mudimanchi <jayakrishna.mudimanchi@ibm.com>
2022-09-28 13:58:54 +0530

    added security

--- src/main/java/com/ibm/org/bian/BianApplication.java
+++ src/main/java/com/ibm/org/bian/BianApplication.java
@@ -3,6 +3,7 @@ package com.ibm.org.bian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 
 @SpringBootApplication
 @ComponentScan(basePackages ="com.ibm.org.bian.*")
@@ -10,6 +11,13 @@ public class BianApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BianApplication.class, args);
+	}
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
